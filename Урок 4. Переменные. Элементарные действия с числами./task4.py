dc6d4c23147efb604018c2a41a32f89573f13cf4
IliaProkopev <157277545+IliaProkopev@users.noreply.github.com>
2024-01-29 21:45:20 +0800

    Add files via upload

--- Урок 4. Переменные. Элементарные действия с числами./task4.py
+++ Урок 4. Переменные. Элементарные действия с числами./task4.py
@@ -1,1 +1,16 @@
 # задание 4
+name = int(input())
+# a - кол-во сотен
+# b - кол-во десятков
+# c - кол-во единиц 
+if name<0:
+    name = name*(-1)
+a = name//100
+b = name % 100 // 10
+c = name % 10
+print(name)
+print(a*100+c*10+b)
+print(b*100+a*10+c)
+print(b*100+c*10+a)
+print(c*100+a*10+b)
+print(c*100+b*10+a)

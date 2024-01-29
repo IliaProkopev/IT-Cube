592677f7207e47f2232cd0ca9baab98134fcd5ae
IliaProkopev <157277545+IliaProkopev@users.noreply.github.com>
2024-01-29 22:25:34 +0800

    Add files via upload

--- Урок 3. Логические выражения и операторы. Условный оператор./task6.py
+++ Урок 3. Логические выражения и операторы. Условный оператор./task6.py
@@ -1,1 +1,9 @@
 # задание 6
+a = int(input()) # a исходный столбец
+b = int(input()) # b исходная строка 
+a1 = int(input()) # a1 конечный столбец
+b1 = int(input()) # b1 конечная строка
+if a1 == a or b == b1 :
+    print("да")
+else :
+    print("нет")       

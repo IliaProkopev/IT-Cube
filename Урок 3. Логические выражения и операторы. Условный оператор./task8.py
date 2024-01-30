87b39885b13014fd6fe99916205a874c470de900
IliaProkopev <157277545+IliaProkopev@users.noreply.github.com>
2024-01-30 21:00:40 +0800

    Add files via upload

---
+++ Урок 3. Логические выражения и операторы. Условный оператор./task8.py
@@ -0,0 +1,16 @@
+# задание 2 за 28 число
+a, b = int(input()), int(input())
+s = input()
+if s == '+':
+    print(a + b)
+elif s == '-':
+    print(a - b)
+elif s == '*':
+    print(a * b)
+elif s == '/':
+    if b == 0:
+        print('На ноль делить нельзя!')
+    else:
+        print(a / b)
+else:
+    print('Неверная операция')        
